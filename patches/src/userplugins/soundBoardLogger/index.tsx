b147716857c071cf580ec6b9e14011c5d879bf40
BlueberryWolf <blue@blueberry.coffee>
2024-09-03 02:51:08 -0600

    added a couple more plugins from equicord

---
+++ patches/src/userplugins/soundBoardLogger/index.tsx
@@ -0,0 +1,65 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { addChatBarButton, removeChatBarButton } from "@api/ChatButtons";
+import { disableStyle, enableStyle } from "@api/Styles";
+import ErrorBoundary from "@components/ErrorBoundary";
+import { Devs, EquicordDevs } from "@utils/constants";
+import definePlugin from "@utils/types";
+import { FluxDispatcher } from "@webpack/common";
+
+import { ChatBarIcon } from "./components/Icons";
+import { OpenSBLogsButton } from "./components/SoundBoardLog";
+import settings from "./settings";
+import { updateLoggedSounds } from "./store";
+import styles from "./styles.css?managed";
+import { getListeners } from "./utils";
+
+export default definePlugin({
+    name: "SoundBoardLogger",
+    authors: [Devs.Moxxie, EquicordDevs.Fres, Devs.echo, EquicordDevs.thororen],
+    description: "Logs all soundboards that are played in a voice chat and allows you to download them",
+    dependencies: ["ChatInputButtonAPI"],
+    patches: [
+        {
+            predicate: () => settings.store.IconLocation === "toolbar",
+            find: "toolbar:function",
+            replacement: {
+                match: /(function \i\(\i\){)(.{1,200}toolbar.{1,200}mobileToolbar)/,
+                replace: "$1$self.addSBIconToToolBar(arguments[0]);$2"
+            }
+        }
+    ],
+    settings,
+    addSBIconToToolBar(e: { toolbar: React.ReactNode[] | React.ReactNode; }) {
+        if (Array.isArray(e.toolbar))
+            return e.toolbar.push(
+                <ErrorBoundary noop={true}>
+                    <OpenSBLogsButton />
+                </ErrorBoundary>
+            );
+
+        e.toolbar = [
+            <ErrorBoundary noop={true}>
+                <OpenSBLogsButton />
+            </ErrorBoundary>,
+            e.toolbar,
+        ];
+    },
+    start() {
+        enableStyle(styles);
+        FluxDispatcher.subscribe("VOICE_CHANNEL_EFFECT_SEND", async sound => {
+            if (!sound?.soundId) return;
+            await updateLoggedSounds(sound);
+            getListeners().forEach(cb => cb());
+        });
+        if (settings.store.IconLocation === "chat") addChatBarButton("vc-soundlog-button", ChatBarIcon);
+    },
+    stop() {
+        disableStyle(styles);
+        if (settings.store.IconLocation === "chat") removeChatBarButton("vc-soundlog-button");
+    }
+});
---
+++ patches/src/userplugins/youtubeDescription/index.tsx
@@ -0,0 +1,50 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import ErrorBoundary from "@components/ErrorBoundary";
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+import { useState } from "@webpack/common";
+import { Embed } from "discord-types/general";
+
+interface ToggleableDescriptionProps { embed: Embed, original: () => any; }
+
+export default definePlugin({
+    name: "YoutubeDescription",
+    description: "Adds descriptions to youtube video embeds",
+    authors: [Devs.arHSM],
+    patches: [
+        {
+            find: ".Messages.SUPPRESS_ALL_EMBEDS",
+            replacement: {
+                match: /case (\i\.\i\.VIDEO):(case \i\.\i\.\i:)*break;default:(\i)=(?:(this\.renderDescription)\(\))\}/,
+                replace: "$2 break; case $1: $3 = $self.ToggleableDescriptionWrapper({ embed: this.props.embed, original: $4.bind(this) }); break; default: $3 = $4() }"
+            }
+        }
+    ],
+    ToggleableDescription: ErrorBoundary.wrap(({ embed, original }: ToggleableDescriptionProps) => {
+        const [isOpen, setOpen] = useState(false);
+
+        if (!embed.rawDescription)
+            return null;
+        if (embed.rawDescription.length <= 20)
+            return original();
+
+        return (
+            <div
+                style={{ cursor: "pointer", marginTop: isOpen ? "0px" : "8px" }}
+                onClick={() => setOpen(o => !o)}
+            >
+                {isOpen
+                    ? original()
+                    : embed.rawDescription.substring(0, 20) + "..."}
+            </div>
+        );
+    }),
+    ToggleableDescriptionWrapper(props: ToggleableDescriptionProps) {
+        return <this.ToggleableDescription {...props} ></this.ToggleableDescription >;
+    }
+});
